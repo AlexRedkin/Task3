9817e4ce62ccd42d51f1d8a9f1d2d1ef5912a654
AlexRedkin <powerredkin@gmail.com>
2018-01-30 05:53:14 +0300

    iteration1
    iteration1

--- start.ps1
+++ start.ps1
@@ -2,11 +2,11 @@
 Login-AzureRmAccount
 
 Get-AzureRmSubscription
-Set-AzureRmContext -SubscriptionName "nmae sub"
+#Set-AzureRmContext -SubscriptionName "nmae sub" - I have only one subscription
 
 New-AzureRmResourceGroup -Name "azuretest" -Location "eastus"
 
 
 
 
-New-AzureRmResourceGroupDeployment -Name "New infrastracture" -ResourceGroupName "azuretest" -TemplateFile "D:\AzureLab3\Main.json"
+New-AzureRmResourceGroupDeployment -Name "new_infrastracture" -ResourceGroupName "azuretest" -TemplateFile ".\Main.json"
